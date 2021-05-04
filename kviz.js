afa22e8bc4295fe00ac050d3dab17b6641eff9ac
Hana Rašovská <hana.rasovska@gmail.com>
2021-05-04 10:18:26 +0200

    definice poleOtazek

---
+++ kviz.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let poleOtazek = [
+  {otazka: "Jakou barvu má rajče?", prvniOdpoved:"červenou",druhaOdpoved:"bílou",tretiOdpoved:"modrou"},
+  {otazka: "Jakou barvu má banán?", prvniOdpoved:"růžovou",druhaOdpoved:"oranžovou",tretiOdpoved:"žlutou"},
+  {otazka: "Jakou barvu má hrášek?", prvniOdpoved:"červenou",druhaOdpoved:"modrou",tretiOdpoved:"zelenou"},
+];
