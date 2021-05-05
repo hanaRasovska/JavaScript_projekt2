141d04a29a121e63535f392ff5c1ac21c52254e0
Hana Rašovská <hana.rasovska@gmail.com>
2021-05-05 09:47:47 +0200

    prvni divy

--- kviz.js
+++ kviz.js
@@ -1,7 +1,20 @@
 "use strict";
 
-let poleOtazek = [
-  {otazka: "Jakou barvu má rajče?", prvniOdpoved:"červenou",druhaOdpoved:"bílou",tretiOdpoved:"modrou"},
-  {otazka: "Jakou barvu má banán?", prvniOdpoved:"růžovou",druhaOdpoved:"oranžovou",tretiOdpoved:"žlutou"},
-  {otazka: "Jakou barvu má hrášek?", prvniOdpoved:"červenou",druhaOdpoved:"modrou",tretiOdpoved:"zelenou"},
+// deklarace promennych
+let poleOtazek;
+let kviz;
+let otazka;
+
+
+poleOtazek = [
+  {otazka: "Jakou barvu má rajče?", prvniMoznost:"červenou",druhaMoznost:"bílou",tretiMoznost:"modrou"},
+  {otazka: "Jakou barvu má banán?", prvniMoznost:"růžovou",druhaMoznost:"oranžovou",tretiMoznost:"žlutou"},
+  {otazka: "Jakou barvu má hrášek?", prvniMoznost:"červenou",druhaMoznost:"modrou",tretiMoznost:"zelenou"},
 ];
+
+otazka=document.getElementById("otazka");
+
+function zobrazeniOtazky(){
+  otazka=poleOtazek.otazka[0];
+}
+
